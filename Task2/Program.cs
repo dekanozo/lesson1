f3c4ca9fcc1fe7f2403066016cbf3e8d6e0df93d
Timofey Dekanozo <dekanozov@gmail.com>
2024-03-09 19:19:48 +0300

    Initial commit

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 =i1 + l1;
+Console.WriteLine(sum2);
+
+double d1 =5.5;
+double sum3 = i1 + l1;
+Console.WriteLine(sum3);
+
+int product = i1 * i2;
+double product2 = i1 * d1;
+Console.WriteLine(product);
+Console.WriteLine(product2);
+
+int quotient = i1 / i2;
+Console.WriteLine(quotient);
+
+d1 = 6;
+double quotient2 = i1 / d1;
+Console.WriteLine(quotient2);
