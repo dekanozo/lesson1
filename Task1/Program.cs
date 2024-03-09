352355c8a4df2728a165c7c94c6817039fadc8c5
Timofey Dekanozo <dekanozov@gmail.com>
2024-03-09 16:48:21 +0300

    добавили переменные

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿// Console.WriteLine("Hello, World!"); //
 
-namespace Task1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+
+int value = 100;
+Console.WriteLine("100");
+
+double b = 5.55;
+char с = '!';
+bool d = true;
+Console.WriteLine(b);
+Console.WriteLine(с);
+Console.WriteLine(d);
