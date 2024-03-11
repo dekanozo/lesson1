25374862a086fcbe5c1eb9ba36a40cbd1c91a295
Timofey Dekanozo <dekanozov@gmail.com>
2024-03-11 22:00:41 +0300

    добавили homework3

--- homework3/Program.cs
+++ homework3/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число в диапазоне от 10 до 99:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number >= 10 && number <= 99)
+        {
+            int maxDigit = GetMaxDigit(number);
+            Console.WriteLine($"Наибольшая цифра числа {number} - {maxDigit}.");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: Введенное число не находится в диапазоне от 10 до 99.");
+        }
+    }
+
+    static int GetMaxDigit(int number)
+    {
+        string numberString = number.ToString();
+        int maxDigit = int.MinValue;
+
+        foreach (char digit in numberString)
+        {
+            if (digit != '0' && digit > maxDigit)
+            {
+                maxDigit = Convert.ToInt32(digit.ToString());
+            }
+        }
+
+        return maxDigit;
+    }
+}
+
