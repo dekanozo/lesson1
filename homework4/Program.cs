fded7a8e750b4b59a2472dfff9ca5ae78a8c875a
Timofey Dekanozo <dekanozov@gmail.com>
2024-03-11 22:14:52 +0300

    добавили homework4

---
+++ homework4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите натуральное число:");
+        BigInteger number = BigInteger.Parse(Console.ReadLine());
+
+        if (number > 0)
+        {
+            string numberString = number.ToString();
+            Console.WriteLine(string.Join(",", numberString.ToCharArray()));
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: Введенное число не является натуральным.");
+        }
+    }
+}
+
+
