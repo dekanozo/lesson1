4e5d75ce37980e878297557a6db0d77a87b9c277
Timofey Dekanozo <dekanozov@gmail.com>
2024-03-13 22:43:46 +0300

    seminars

---
+++ Seminars/Sem1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        if (firstNumber < secondNumber)
+        {
+            Console.WriteLine($"{secondNumber} больше, чем {firstNumber}");
+        }
+        else if (firstNumber > secondNumber)
+        {
+            Console.WriteLine($"{firstNumber} больше, чем {secondNumber}");
+        }
+        else
+        {
+            Console.WriteLine($"{firstNumber} равно {secondNumber}");
+        }
+    }
+    
+    // Пример использования метода CompareNumbers
+    public static void Main(string[] args)
+    {
+        CompareNumbers(10, 5); // Пример вывода: 10 больше, чем 5
+        CompareNumbers(5, 10); // Пример вывода: 10 больше, чем 5
+        CompareNumbers(5, 5);  // Пример вывода: 5 равно 5
+    }
+}
---
+++ homework1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (IsMultipleOf7And23(number))
+        {
+            Console.WriteLine($"Число {number} кратно одновременно 7 и 23.");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number} не кратно одновременно 7 и 23.");
+        }
+    }
+
+    static bool IsMultipleOf7And23(int number)
+    {
+        return (number % 7 == 0) && (number % 23 == 0);
+    }
+}
+
